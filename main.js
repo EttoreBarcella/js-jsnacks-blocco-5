53b8610f133ca0f2dcc6b2d10c49f5173680afb3
EttoreBarcella <100206174+EttoreBarcella@users.noreply.github.com>
2022-05-16 12:23:58 +0200

    added objects

---
+++ main.js
@@ -0,0 +1,53 @@
+const zucchine = [
+{
+    type : "a",
+    length : 123,
+    weight : 23,
+},
+{
+    type : "b",
+    length : 123,
+    weight : 23,
+},
+{
+    type : "c",
+    length : 123,
+    weight : 23,
+},
+{
+    type : "d",
+    length : 123,
+    weight : 23,
+},
+{
+    type : "e",
+    length : 123,
+    weight : 23,
+},
+{
+    type : "f",
+    length : 123,
+    weight : 23,
+},
+{
+    type : "g",
+    length : 123,
+    weight : 23,
+},
+{
+    type : "h",
+    length : 123,
+    weight : 23,
+},
+{
+    type : "i",
+    length : 123,
+    weight : 23,
+},
+{
+    type : "a",
+    length : 123,
+    weight : 23,
+}
+
+]
